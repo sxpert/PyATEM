8e09864ce25d4715c36327c220cbdd45ad6746e7
mraerino <melodie124@gmail.com>
2015-11-16 02:36:08 +0100

    #WIP Catching value change in dicts to send to switcher

--- atem.py
+++ atem.py
@@ -4,6 +4,7 @@
 
 import socket
 import struct
+import collections
 
 
 def dumpHex (buffer):
@@ -24,86 +25,24 @@ def dumpAscii (buffer):
 
 
 # Handles changes in attributes of the Atem class
-class AttrProxy(object):
-    __slots__ = ["_obj", "__weakref__"]
-    def __init__(self, obj, atem, name):
-        self.atem = atem
-        self.name = name
-        object.__setattr__(self, "_obj", obj)
+class DictProxy(collections.UserDict):
+    """
+    :type _atem: Atem
+    """
+    _atem = None
+    _name = []
+    def __init__(self, data, atem, name):
+        self._atem = atem
+        self._name = name
+        collections.UserDict.__init__(self, data)
 
-    #
-    # proxying (special cases)
-    #
-    def __getattribute__(self, name):
-        return getattr(object.__getattribute__(self, "_obj"), name)
-    def __delattr__(self, name):
-        delattr(object.__getattribute__(self, "_obj"), name)
-    def __setattr__(self, name, value):
-        # settings happens here
-        print("'" + name + "' has been changed.")
-        setattr(object.__getattribute__(self, "_obj"), name, value)
-
-    def __nonzero__(self):
-        return bool(object.__getattribute__(self, "_obj"))
-    def __str__(self):
-        return str(object.__getattribute__(self, "_obj"))
-    def __repr__(self):
-        return repr(object.__getattribute__(self, "_obj"))
-
-    #
-    # factories
-    #
-    _special_names = [
-        '__abs__', '__add__', '__and__', '__call__', '__cmp__', '__coerce__',
-        '__contains__', '__delitem__', '__delslice__', '__div__', '__divmod__',
-        '__eq__', '__float__', '__floordiv__', '__ge__', '__getitem__',
-        '__getslice__', '__gt__', '__hash__', '__hex__', '__iadd__', '__iand__',
-        '__idiv__', '__idivmod__', '__ifloordiv__', '__ilshift__', '__imod__',
-        '__imul__', '__int__', '__invert__', '__ior__', '__ipow__', '__irshift__',
-        '__isub__', '__iter__', '__itruediv__', '__ixor__', '__le__', '__len__',
-        '__long__', '__lshift__', '__lt__', '__mod__', '__mul__', '__ne__',
-        '__neg__', '__oct__', '__or__', '__pos__', '__pow__', '__radd__',
-        '__rand__', '__rdiv__', '__rdivmod__', '__reduce__', '__reduce_ex__',
-        '__repr__', '__reversed__', '__rfloorfiv__', '__rlshift__', '__rmod__',
-        '__rmul__', '__ror__', '__rpow__', '__rrshift__', '__rshift__', '__rsub__',
-        '__rtruediv__', '__rxor__', '__setitem__', '__setslice__', '__sub__',
-        '__truediv__', '__xor__', 'next',
-    ]
-
-    @classmethod
-    def _create_class_proxy(cls, theclass):
-        """creates a proxy for the given class"""
-
-        def make_method(name):
-            def method(self, *args, **kw):
-                return getattr(object.__getattribute__(self, "_obj"), name)(*args, **kw)
-            return method
-
-        namespace = {}
-        for name in cls._special_names:
-            if hasattr(theclass, name):
-                namespace[name] = make_method(name)
-        return type("%s(%s)" % (cls.__name__, theclass.__name__), (cls,), namespace)
-
-    def __new__(cls, obj, *args, **kwargs):
-        """
-        creates an proxy instance referencing `obj`. (obj, *args, **kwargs) are
-        passed to this class' __init__, so deriving classes can define an
-        __init__ method of their own.
-        note: _class_proxy_cache is unique per deriving class (each deriving
-        class must hold its own cache)
-        """
-        try:
-            cache = cls.__dict__["_class_proxy_cache"]
-        except KeyError:
-            cls._class_proxy_cache = cache = {}
-        try:
-            theclass = cache[obj.__class__]
-        except KeyError:
-            cache[obj.__class__] = theclass = cls._create_class_proxy(obj.__class__)
-        ins = object.__new__(theclass)
-        theclass.__init__(ins, obj, *args, **kwargs)
-        return ins
+    # proxying dict setting
+    def __setitem__(self, key, value):
+        if isinstance(value, dict):
+            self.data[key] = DictProxy(value, self._atem, self._name+[key])
+        else:
+            if self._atem.attrChange(self._name+[key], value):
+                self.data[key] = value
 
 
 # implements communication with atem switcher
@@ -144,23 +83,6 @@ class Atem:
     VALUES_CC_SHUTTER = {20000: '1/50', 16667: '1/60', 13333: '1/75', 11111: '1/90', 10000: '1/100', 8333: '1/120', 6667: '1/150', 5556: '1/180', 4000: '1/250', 2778: '1/360', 2000: '1/500', 1379: '1/725', 1000: '1/1000', 690: '1/1450', 500: '1/2000'}
     VALUES_AUDIO_MIX = { 0: 'off', 1: 'on', 2: 'AFV' }
 
-    system_config = { 'inputs': {}, 'audio': {} }
-    status = {}
-    config = { 'multiviewers': {}, 'mediapool': {} }
-    state = {
-        'program': {},
-        'preview': {},
-        'keyers': {},
-        'dskeyers': {},
-        'aux': {},
-        'mediaplayer': {},
-        'mediapool': {},
-        'audio': {},
-        'tally_by_index': {},
-        'tally': {}
-    }
-    cameracontrol = {}
-
     # initializes the class
     def __init__(self, address):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -172,6 +94,25 @@ class Atem:
         self.packetCounter = 0
         self.isInitialized = False
         self.currentUid = 0x1337
+
+        self.system_config = { 'inputs': {}, 'audio': {} }
+        self.status = {}
+        self.config = { 'multiviewers': {}, 'mediapool': {} }
+        self.state = {
+            'program': {},
+            'preview': {},
+            'keyers': {},
+            'dskeyers': {},
+            'aux': {},
+            'mediaplayer': {},
+            'mediapool': {},
+            'audio': {},
+            'tally_by_index': {},
+            'tally': {}
+        }
+        self.cameracontrol = {}
+
+        self.state['program']['booted'] = True
 
     # hello packet
     def connectToSwitcher(self):
@@ -580,10 +521,14 @@ class Atem:
     # -----------------------------
     def __setattr__(self, name, value):
         if name in ['system_config', 'status', 'config', 'state', 'cameracontrol']:
-            self.__dict__[name] = AttrProxy(value, self, name)
-            print("object setting proxied")
+            self.__dict__[name] = DictProxy(value, self, [name])
         else:
             self.__dict__[name] = value
+
+    def attrChange(self, name, value):
+        print("'" + '/'.join(name) + "' has changed")
+        # todo: implement communication of value change to the switcher
+        return True
 
 if __name__ == '__main__':
     import config
